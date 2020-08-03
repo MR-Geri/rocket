069e70db733351b6e3fbbb133013809f73c042d8
MR-Geri <63905039+MR-Geri@users.noreply.github.com>
2020-08-03 22:47:26 +0300

    main

--- main.py
+++ main.py
@@ -66,11 +66,14 @@ def main():
         v_main_y.append(V_y)
         v.append(np.sqrt(V_x ** 2 + V_y ** 2))
 
-    fig.add_trace(go.Scatter(x=x_m, y=y_m, name='$$y=f(x)$$'))
-    fig.add_trace(go.Scatter(x=time, y=v_main_x, name='$$V_y(t)$$'))
-    fig.add_trace(go.Scatter(x=time, y=v_main_y, name='$$V_x(t)$$'))
-    fig.add_trace(go.Scatter(x=time, y=v, name='$$V(t)$$'))
+    fig.add_trace(go.Scatter(x=x_m, y=y_m, name='y=f(x)'))
+    fig.add_trace(go.Scatter(x=time, y=x_m, name='x(t)'))
+    fig.add_trace(go.Scatter(x=time, y=y_m, name='y(t)'))
+    fig.add_trace(go.Scatter(x=time, y=v_main_x, name='V_y(t)'))
+    fig.add_trace(go.Scatter(x=time, y=v_main_y, name='V_x(t)'))
+    fig.add_trace(go.Scatter(x=time, y=v, name='V(t)'))
     plotly.offline.plot(fig, filename='file.html')
+    fig.update_layout(legend_orientation="h")
     fig.show()
 
 
